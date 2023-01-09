50434749407dd7e2f20105d00222a7a6da856763
Nazareno Luna <nazarenogluna12@hotmail.com>
2023-01-09 15:09:02 -0300

    solucionando errores en urls de la app y base.html

--- estudiantes/urls.py
+++ estudiantes/urls.py
@@ -5,6 +5,6 @@ from estudiantes.views import saludar, listar_estudiantes,listar_profesores
 
 urlpatterns = [
     path('saludar/', saludar),
-    path('lista-alumnos/', listar_estudiantes, name="lista_alumnos"),
-    path('saludar3/', listar_profesores, name="listar_profesores"),
+    path('lista-alumnos/', listar_estudiantes, name="lista_estudiantes"),
+    path('saludar3/', listar_profesores, name="lista_profesores"),
 ]
